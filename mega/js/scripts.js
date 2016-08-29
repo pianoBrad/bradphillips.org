90cbc0ca6e62353bd54c64ddc227cea8bb155471
Brad Phillips <brad@bradphillips.org>
2016-08-29 17:55:45 -0500

    Update mega js for word update functionality.

--- mega/js/scripts.js
+++ mega/js/scripts.js
@@ -1,8 +1,19 @@
-var setTheme = function($element) {
-	var theme_classes = ['yellow', 'blue'];
-	var random_number = Math.floor(Math.random()*theme_classes.length);
+var theme_classes = ['yellow', 'blue'];
 
+var setTheme = function($element) {
+    var random_number = Math.floor(Math.random()*theme_classes.length);
+    
 	$element.addClass(theme_classes[random_number]);	
+}
+
+var changeTheme = function($element) {
+    var random_number = Math.floor(Math.random()*theme_classes.length);
+
+    $(theme_classes).each(function(index) {
+        $element.removeClass(theme_classes[index]);
+    });
+
+    $element.addClass(theme_classes[random_number]);    
 }
 
 var clickOrTouch = function() {
@@ -19,31 +30,46 @@ var pressTile = function($element) {
     });
 }
 
-var updateTilesWord = function(theWord) {
+var updateTilesWord = function($clicked_element, theWord) {
 	var $elements = $('.tile');
-	var length = $elements.length;
+	var $clicked_element_index = $clicked_element.index();
+    var length = $elements.length;
 	var index = 0;
 	var speed = 150;
-	
-	var timer = setInterval(replaceWord, speed);
 
-	function replaceWord() {
+    var single_timer = setInterval(replaceLetter("single"), speed);
+	var timer = setInterval(replaceLetter, speed);
 
-		var htmlContent = '';
+	function replaceLetter(mode="plural") {
+      
+        if (mode != "plural") { 
+            index = $clicked_element_index; 
+        } else if (mode == "plural" && index == 0 && $clicked_element_index == 0) {
+            index = 1;    
+        }
+
+        var htmlContent = '';
 
         if (typeof theWord[index] !== 'undefined') {
-            console.log('yah!');
             htmlContent = '<span class="dropshadow-'+theWord[index]+'">'+theWord[index]+'</span>';
         }
 
         pressTile($($elements.get(index))); 
         $($elements.get(index)).find('.letter').html(htmlContent);
 
-		index++;
+		if (mode != "plural") { index = 0; 
+        } else if ( mode == "plural" && index == ($clicked_element_index-1) ){
+            index = index+2;
+        } else { index++; }
 
         // remove timer after interating through all articles
-        if (index >= length) {
-            clearInterval(timer);
+        if (mode != "plural") {
+            if (index >= length) {
+                changeTheme($('.tiles-wrap'));
+                clearInterval(timer);
+            }
+        } else {
+            clearInterval(single_timer);
         }
 	}
 }
@@ -57,7 +83,9 @@ var uiNext = function($element) {
 	$('html').removeClass('live');
 
 	if ($element.hasClass('tile')) {
-		$element.find('.tile-click-overlay').addClass('selected').delay(150).queue(function(next) {
+	    var next_word = $element.data('nextWord');
+        /**
+        $element.find('.tile-click-overlay').addClass('selected').delay(150).queue(function(next) {
 			$(this).removeClass('selected').dequeue();
 			var next_word = $element.data('nextWord');
 			if (typeof next_word !== 'undefined') {
@@ -66,6 +94,13 @@ var uiNext = function($element) {
 				goLive();
 			}
 		});
+        **/
+        if (typeof next_word !== 'undefined') {
+            updateTilesWord($element, next_word);
+        } else {
+            pressTile($element);
+            goLive();
+        }
 	}
 
 }
