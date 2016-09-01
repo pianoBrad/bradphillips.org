a9c6ad3b448093a1568a33aae2447266817ce638
Brad Phillips <brad@bradphillips.org>
2016-09-01 17:23:55 -0500

    Update mega to add initial hammer js pan listeners.

--- mega/js/scripts.js
+++ mega/js/scripts.js
@@ -1,4 +1,6 @@
 var theme_classes = ['yellow', 'blue'];
+var hammerInstances = [];
+var numCols = 3;
 
 var setTheme = function($element) {
     var random_number = Math.floor(Math.random()*theme_classes.length);
@@ -81,10 +83,13 @@ var updateTilesWord = function($clicked_element, theWord) {
 var goLive = function() {
 	$('html').removeClass('ui-updating').addClass('live');
 }
+var stopLive = function() {
+	$('html').removeClass('live').addClass('ui-updating');
+}
 
 var uiNext = function($element) {
 	
-	$('html').removeClass('live').addClass('ui-updating');
+	stopLive();	
 
 	if ($element.hasClass('tile')) {
 	    var next_word = $element.data('nextWord');
@@ -98,15 +103,113 @@ var uiNext = function($element) {
 
 }
 
+function dirProp(direction, hProp, vProp) {
+    return (direction & Hammer.DIRECTION_HORIZONTAL) ? hProp : vProp
+}
+
+var moveTileSet = function(elements, percent, dir) {
+	//console.log(percent);
+	$.each(elements, function() {
+		if (percent < (100/numCols) && percent > -(100/numCols)) { 
+			$(this).css({
+				'left' : percent+'%'
+			});
+		} else if (percent < -(100/numCols) && percent < 0) {
+			$(this).css({
+                'left' : '-'+percent+'%'
+            });
+		} else if (percent < -(100/numCols)) {
+			$(this).css({
+                'left' : '-33.333%'
+            });
+		} else {
+			$(this).css({
+				'left' : '33.333%'
+			});
+		}
+	});
+}
+
+getMatchingSiblings = function($element, $searchSet) {
+	var tiles = [];
+	$searchSet.each(function() {
+        if($(this).offset().top == $element.offset().top) {
+            tiles.push($(this));
+		}
+    });
+	return tiles;
+}
+
+var tileHammerHandler = function(tileId, ev) {
+	stopLive();	
+
+	$tile = $('#'+tileId);	
+	
+	var tileW = $tile.outerWidth();
+	var delta = dirProp(ev.direction, ev.deltaX, ev.deltaY);
+    var percent = ((100 / tileW) * delta) / numCols;
+    var animate = false;
+
+	//console.log(percent);
+	$allTiles = $tile.siblings();
+
+	var tiles = [];
+	tiles.push($tile);
+	//console.log(ev.type);
+	
+	switch (ev.type) {
+		case "panleft":
+			tiles = $.merge(tiles, getMatchingSiblings($tile, $allTiles));
+			tiles = $.merge(tiles, getMatchingSiblings($tile, $('.tiles-wrap.nav.left .tile')));
+			moveTileSet(tiles, percent);
+			break;
+		case "panright":
+			tiles = $.merge(tiles,getMatchingSiblings($tile, $allTiles));
+			tiles = $.merge(tiles, getMatchingSiblings($tile, $('.tiles-wrap.nav.left .tile')));
+			moveTileSet(tiles, percent);
+		default:
+			break;
+	}
+}
+
+
+var setUpHammerListeners = function($selector) {
+
+	$selector.each(function() {
+		var tileId = $(this).attr('id');
+
+		var myElement = document.getElementById(tileId);
+
+		// create a simple instance
+		// by default, it only adds horizontal recognizers
+		var mc = new Hammer(myElement);
+
+		// let the pan gesture support all directions.
+		// this will block the vertical scrolling on a touch-device while on the element
+		mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+
+		// listen to events...
+		mc.on("panleft panright panup pandown", function(ev) {
+			//myElement.textContent = ev.type +" gesture detected.";
+			tileHammerHandler(tileId, ev);
+		});
+		hammerInstances.push(mc);
+	});
+
+}
+
+
 $( document ).ready(function() {
 
 setTheme($('.tiles-wrap'));
 
 
-$('.tile').on(clickOrTouch(), function() {
+$('.live .tile').on('click', function() {
 	if ($('.live').length > 0) {
 		uiNext($(this));
 	}
 });
+
+setUpHammerListeners($('.ui .tiles-wrap:not(.nav) .tile'));
 
 });
