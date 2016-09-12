3049f5792447ad231bc786cd875cd6a5663bdb03
Brad Phillips <brad@bradphillips.org>
2016-09-12 01:57:07 -0500

    test ios issues for text detail overlay.

--- mega/js/scripts.js
+++ mega/js/scripts.js
@@ -375,7 +375,7 @@ function addEventListeners() {
 		}
 	
 	});
-	$('body').on('click', '.text-detail', function(e) {
+	$('body').on('click touchend', '.text-detail', function(e) {
 		var $element = $(e.target);		
 		console.log('test');
 
